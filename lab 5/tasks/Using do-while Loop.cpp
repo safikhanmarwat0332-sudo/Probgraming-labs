4b1542e085ce4537654adb408289f2bfffbfca29
safikhanmarwat0332-sudo <safikhanmarwat0332@gmail.com>
2026-04-20 11:32:35 -0700

    Add files via upload

---
+++ lab 5/tasks/Using do-while Loop.cpp
@@ -0,0 +1,24 @@
+    //NAME  :  SAFIULLAH
+    //REG:  BF25NWELE0687
+    //SECTION : B
+    //ELECTRICAL (AI)
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    do
+    {
+        cout << i << endl;
+        i++;
+    }
+    while(i <= 10);
+
+    return 0;
+}
---
+++ lab 5/tasks/While Loop.cpp
@@ -0,0 +1,20 @@
+    //NAME  :  SAFIULLAH
+    //REG:  BF25NWELE0687
+    //SECTION : B
+    //ELECTRICAL (AI)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    while(i <= 10)
+    {
+        cout << i << endl;
+        i++;
+    }
+
+    return 0;
+}
