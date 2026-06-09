6d9740b42a71712dbc9b15561e2032fe56040ad1
safikhanmarwat0332-sudo <safikhanmarwat0332@gmail.com>
2026-06-09 01:08:36 -0700

    Add files via upload

---
+++ Lab 7-/Task 1.cpp
@@ -0,0 +1,14 @@
+
+
+
+#include <iostream>          			// Include input-output library
+using namespace std;         			// Use standard namespace
+int main() {                 				// Main function start
+    int numbers[5] = {10, 20, 30, 40, 50}; 	// Declare and initialize array
+    cout << "Array elements are:\n";       	// Display message
+    for(int i = 0; i < 5; i++) {           		// Loop through array
+        cout << numbers[i] << " ";         	// Print each element
+    }
+    return 0;   					// End program
+}
+
