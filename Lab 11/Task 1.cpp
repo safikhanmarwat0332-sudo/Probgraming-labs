544474116bb415d13232dd4d56c913b47bac1aac
safikhanmarwat0332-sudo <safikhanmarwat0332@gmail.com>
2026-06-09 01:23:22 -0700

    Add files via upload

---
+++ Lab 11/Task 1.cpp
@@ -0,0 +1,17 @@
+
+ 
+ #include <iostream>                               // Header file for input and output 
+    #include <string>                                   // Header file for string functions 
+  using namespace std;                              // Use standard namespace 
+  int main()              // Main function 
+  { 
+     // Declare and initialize a string variable 
+       string message = "Welcome to C++ Programming"; 
+ 
+      // Display the original string 
+      cout << "Message: " << message << endl; 
+ 
+       // Use length() function to calculate string length 
+      cout << "Length of string = " << message.length() << endl; 
+      
+      return 0;}   // End the program successfully 
