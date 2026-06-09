144d597635cfd3201cb6cd5a0a55856f8e814d06
safikhanmarwat0332-sudo <safikhanmarwat0332@gmail.com>
2026-06-09 01:10:04 -0700

    Add files via upload

---
+++ Lab 7-/Tasks/Task 2.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>           				// Include library
+using namespace std;          				// Use namespace
+int main() {                  				// Main function
+    char message[] = "HELLO"; 			// Declare and initialize character array
+    cout << "Characters are:\n"; 			// Display message
+    for(int i = 0; message[i] != '\0'; i++) { 		// Loop until null character
+        cout << message[i] << endl;}        		// Print each character on new line
+    return 0;   						// End program
+}
+
